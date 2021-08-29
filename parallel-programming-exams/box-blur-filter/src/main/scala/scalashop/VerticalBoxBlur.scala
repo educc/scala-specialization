cfd00aa3b3e310d28871df99056bb1cb4691cb04
edu <edward1738@gmail.com>
2021-08-29 14:49:19 -0500

    exam1

---
+++ parallel-programming-exams/box-blur-filter/src/main/scala/scalashop/VerticalBoxBlur.scala
@@ -0,0 +1,73 @@
+package scalashop
+
+import org.scalameter.*
+
+object VerticalBoxBlurRunner:
+
+  val standardConfig = config(
+    Key.exec.minWarmupRuns := 5,
+    Key.exec.maxWarmupRuns := 10,
+    Key.exec.benchRuns := 10,
+    Key.verbose := false
+  ) withWarmer(Warmer.Default())
+
+  def main(args: Array[String]): Unit =
+    val radius = 3
+    val width = 1920
+    val height = 1080
+    val src = Img(width, height)
+    val dst = Img(width, height)
+    val seqtime = standardConfig measure {
+      VerticalBoxBlur.blur(src, dst, 0, width, radius)
+    }
+    println(s"sequential blur time: $seqtime")
+
+    val numTasks = 32
+    val partime = standardConfig measure {
+      VerticalBoxBlur.parBlur(src, dst, numTasks, radius)
+    }
+    println(s"fork/join blur time: $partime")
+    println(s"speedup: ${seqtime.value / partime.value}")
+
+
+/** A simple, trivially parallelizable computation. */
+object VerticalBoxBlur extends VerticalBoxBlurInterface:
+
+  /** Blurs the columns of the source image `src` into the destination image
+   *  `dst`, starting with `from` and ending with `end` (non-inclusive).
+   *
+   *  Within each column, `blur` traverses the pixels by going from top to
+   *  bottom.
+   */
+  def blur(src: Img, dst: Img, from: Int, end: Int, radius: Int): Unit = {
+    val listVertical = (0 until src.height).toSeq
+    def blurToCol(col: Int): Unit = {
+      listVertical.foreach { y =>
+        val pixel = boxBlurKernel(src, col, y, radius)
+        dst.update(col, y, pixel)
+      }
+    }
+    (from until end).foreach(blurToCol)
+  }
+
+  /** Blurs the columns of the source image in parallel using `numTasks` tasks.
+   *
+   *  Parallelization is done by stripping the source image `src` into
+   *  `numTasks` separate strips, where each strip is composed of some number of
+   *  columns.
+   */
+  def parBlur(src: Img, dst: Img, numTasks: Int, radius: Int): Unit = {
+    require(numTasks >= 1)
+    require(numTasks <= src.width)
+
+    val slides = src.width / numTasks
+    val lst = (0 to src.width by slides)
+    lst.zip(lst.tail)
+      .foreach { (left, right) =>
+        println(s"left=$left right=$right")
+        task {
+          blur(src, dst, left, right, radius)
+        }
+      }
+  }
+
